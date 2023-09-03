cb4874824d2f4d9da41424870f32092aa4c19175
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-09-03 16:11:07 -0400

    tests passing

--- binary-tree.js
+++ binary-tree.js
@@ -17,13 +17,45 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
-
+    let start = this.root;
+    function countBranch(node){
+      if(!node) return 0
+      return 1+countBranch(node.left)
+    }
+    return countBranch(start)
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    let maxTravels = 0;
+
+    // Helper function to recursively explore the tree.
+    const findMaxPath = (node) => {
+      if (!node) {
+        return 0;
+      }
+
+      // Recursively calculate the maximum path sum for the left and right subtrees.
+      const leftMax = Math.max(0, findMaxPath(node.left));
+      const rightMax = Math.max(0, findMaxPath(node.right));
+
+      // Calculate the maximum path that includes the current node.
+      const currentMax = leftMax + rightMax;
+
+      // Update the global maximum path if the current path is greater.
+      maxTravels = Math.max(maxTravels, currentMax);
+
+      // Return the maximum path sum that can be extended from this node.
+      return 1 + Math.max(leftMax, rightMax);
+    };
+
+    // Start the traversal from the root node.
+    if(!this.root) return 0
+    findMaxPath(this.root);
+
+    return maxTravels;
 
   }
 
@@ -31,6 +63,33 @@ class BinaryTree {
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let maxSum = Number.MIN_SAFE_INTEGER;
+
+    // Helper function to recursively explore the tree.
+    const findMaxPathSum = (node) => {
+      if (!node) {
+        return 0;
+      }
+
+      // Recursively calculate the maximum path sum for the left and right subtrees.
+      const leftMax = Math.max(0, findMaxPathSum(node.left));
+      const rightMax = Math.max(0, findMaxPathSum(node.right));
+
+      // Calculate the maximum path sum that includes the current node.
+      const currentMax = node.val + leftMax + rightMax;
+
+      // Update the global maximum sum if the current path is greater.
+      maxSum = Math.max(maxSum, currentMax);
+
+      // Return the maximum path sum that can be extended from this node.
+      return node.val + Math.max(leftMax, rightMax);
+    };
+
+    // Start the traversal from the root node.
+    if(!this.root) return 0
+    findMaxPathSum(this.root);
+
+    return maxSum;
 
   }
 
@@ -38,7 +97,24 @@ class BinaryTree {
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if(!this.root){
+      return null
+    }
+    let candidate = this.root.val
 
+    function findNextLarger(node){
+      if(!node) return null
+      const lookLeft = findNextLarger(node.left)
+      const lookRight = findNextLarger(node.right)
+      candidate = node.val > lowerBound ? Math.min(node.val, candidate) : candidate
+      return candidate
+    }
+    let search = findNextLarger(this.root)
+    if(search == lowerBound){
+      return null
+    } else {
+      return search
+    }
   }
 
   /** Further study!
